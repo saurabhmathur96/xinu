e5a0bf5d4c9afce73c9d509c78da0c72d2b9134a
Saurabh Mathur <samathur@iu.edu>
2018-10-24 10:46:55 -0400

    fix errors in xsh_malloc_test

--- shell/xsh_malloc_test.c
+++ shell/xsh_malloc_test.c
@@ -1,22 +1,22 @@
-/* xsh_hello.c - xsh_hello */
+/* xsh_malloc_test.c - xsh_malloc_test */
 
 #include <xinu.h>
 #include <stdio.h>
 
 shellcmd xsh_malloc_test(int nargs, char *args[])
 {
-    void *b1 = malloc(40);
+    void *b1 = xmalloc(40);
     printf(heap_snapshot());
-    void *b2 = malloc(2);
+    void *b2 = xmalloc(2);
     printf(heap_snapshot());
-    void *b3 = malloc(8);
+    void *b3 = xmalloc(8);
     printf(heap_snapshot());
 
-    free(b1);
+    xfree(b1);
     printf(heap_snapshot());
-    free(b2);
+    xfree(b2);
     printf(heap_snapshot());
-    free(b3);
+    xfree(b3);
     printf(heap_snapshot());
 
     return SHELL_OK;
